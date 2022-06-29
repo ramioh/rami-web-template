5f7839fcdf570db781563086dc683e8297f880b0
Rami Hassan:q <48259541+ramioh@users.noreply.github.com>
2022-06-29 17:52:17 +0200

    Add dependencies

--- weather-forecast/2.Infrastructure/Persistence/Repository.cs
+++ weather-forecast/2.Infrastructure/Persistence/Repository.cs
@@ -1,7 +1,9 @@
+using Company.Product.Common.DependencyInjection;
 using Company.Product.WeatherForecast.Persistence.Contracts;
 
 namespace Company.Product.WeatherForecast.Persistence;
 
+[RegisterType(Lifetime.Singleton, typeof(IRepository))]
 public class Repository : IRepository
 {
     public string GetHello()
--- weather-forecast/3.Application/Application/Service.cs
+++ weather-forecast/3.Application/Application/Service.cs
@@ -1,8 +1,10 @@
+using Company.Product.Common.DependencyInjection;
 using Company.Product.WeatherForecast.Application.Contracts;
 using Company.Product.WeatherForecast.Persistence.Contracts;
 
 namespace Company.Product.WeatherForecast.Application;
 
+[RegisterType(Lifetime.Scoped, typeof(IService))]
 public class Service : IService
 {
     private readonly IRepository _repository;
